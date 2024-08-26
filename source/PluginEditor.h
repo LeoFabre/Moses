b3f6ac2bd8475624cf95292feeba4af99ae110f5
Leo Fabre <fabre.leo@gmail.com>
2024-08-26 22:47:34 +0200

    Starting over from IEM Multiband compressor code

--- source/PluginEditor.h
+++ source/PluginEditor.h
@@ -1,31 +1,133 @@
+/*
+ ==============================================================================
+ This file is part of the IEM plug-in suite.
+ Author: Markus Huber
+ Copyright (c) 2017 - Institute of Electronic Music and Acoustics (IEM)
+ https://iem.at
+
+ The IEM plug-in suite is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ The IEM plug-in suite is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this software.  If not, see <https://www.gnu.org/licenses/>.
+ ==============================================================================
+ */
+
 #pragma once
 
-#include "FreqSlider.h"
-#include "GainSlider.h"
 #include "PluginProcessor.h"
 
+#include "lookAndFeel/IEM_LaF.h"
+//Plugin Design Essentials
+#include "Components/TitleBar.h"
+
+//Custom juce::Components
+#include "Components/CompressorVisualizer.h"
+#include "Components/FilterVisualizer.h"
+#include "Components/LevelMeter.h"
+#include "Components/ReverseSlider.h"
+#include "Components/RoundButton.h"
+#include "Components/SimpleLabel.h"
+#include "FilterBankVisualizer.h"
+#include "MasterControl.h"
+
+using SliderAttachment = ReverseSlider::
+    SliderAttachment; // all ReverseSliders will make use of the parameters' valueToText() function
+using ComboBoxAttachment = juce::AudioProcessorValueTreeState::ComboBoxAttachment;
+using ButtonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
+
 //==============================================================================
-class PluginEditor
-        : public juce::AudioProcessorEditor
+/**
+*/
+class MultiBandCompressorAudioProcessorEditor : public juce::AudioProcessorEditor,
+                                                private juce::Timer,
+                                                public juce::Slider::Listener,
+                                                public juce::Button::Listener
 {
 public:
-    explicit PluginEditor(PluginProcessor &);
-
-    ~PluginEditor() override;
+    MultiBandCompressorAudioProcessorEditor (MultiBandCompressorAudioProcessor&,
+                                             juce::AudioProcessorValueTreeState&);
+    ~MultiBandCompressorAudioProcessorEditor();
 
     //==============================================================================
-    void paint(juce::Graphics &) override;
-
+    void paint (juce::Graphics&) override;
     void resized() override;
 
+    void sliderValueChanged (juce::Slider* slider) override;
+    void buttonClicked (juce::Button* bypassButton) override;
+
+    void timerCallback() override;
+
 private:
-    PluginProcessor &processorRef;
+    // ====================== begin essentials ==================
+    // lookAndFeel class with the IEM plug-in suite design
+    LaF globalLaF;
 
-    static constexpr int numBands = 5;  // Nombre de bandes pour le crossover
-    juce::OwnedArray<GainSlider> gainSliders;
-    juce::OwnedArray<FreqSlider> freqSliders;
+    // stored references to the AudioProcessor and juce::ValueTreeState holding all the parameters
+    MultiBandCompressorAudioProcessor& processor;
+    juce::AudioProcessorValueTreeState& valueTreeState;
 
-    void createSliders();
+    /* title and footer component
+     title component can hold different widgets for in- and output:
+        - NoIOWidget (if there's no need for an input or output widget)
+        - AudioChannelsIOWidget<maxNumberOfChannels, isChoosable>
+        - AmbisonicIOWidget<maxOrder>
+        - DirectivitiyIOWidget
+     */
+    TitleBar<AmbisonicIOWidget<>, NoIOWidget> title;
+    OSCFooter footer;
+    // =============== end essentials ============
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PluginEditor)
+    std::unique_ptr<ComboBoxAttachment> cbNormalizationAtachement;
+    std::unique_ptr<ComboBoxAttachment> cbOrderAtachement;
+
+    FilterBankVisualizer<double> filterBankVisualizer;
+    juce::TooltipWindow tooltips;
+
+    // Filter Crossovers
+    ReverseSlider slCrossover[numFilterBands - 1];
+    std::unique_ptr<SliderAttachment> slCrossoverAttachment[numFilterBands - 1];
+
+    // Solo and Bypass juce::Buttons
+    RoundButton tbSolo[numFilterBands];
+    RoundButton tbBypass[numFilterBands];
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment>
+        soloAttachment[numFilterBands], bypassAttachment[numFilterBands];
+
+    // Compressor Parameters
+    ReverseSlider slKnee[numFilterBands], slThreshold[numFilterBands], slRatio[numFilterBands],
+        slAttackTime[numFilterBands], slReleaseTime[numFilterBands], slMakeUpGain[numFilterBands];
+    std::unique_ptr<SliderAttachment> slKneeAttachment[numFilterBands],
+        slThresholdAttachment[numFilterBands], slRatioAttachment[numFilterBands],
+        slAttackTimeAttachment[numFilterBands], slReleaseTimeAttachment[numFilterBands],
+        slMakeUpGainAttachment[numFilterBands];
+
+    // Master parameters
+    juce::GroupComponent gcMasterControls;
+    MasterControl slMasterThreshold, slMasterMakeUpGain, slMasterKnee, slMasterRatio,
+        slMasterAttackTime, slMasterReleaseTime;
+
+    // Compressor Visualization
+    juce::OwnedArray<CompressorVisualizer> compressorVisualizers;
+
+    // Meters
+    LevelMeter GRmeter[numFilterBands], omniInputMeter, omniOutputMeter;
+
+    // juce::Toggle juce::Buttons
+    juce::ToggleButton tbOverallMagnitude;
+    bool displayOverallMagnitude { false };
+
+    // juce::Labels
+    SimpleLabel lbKnee[numFilterBands + 1], lbThreshold[numFilterBands + 1],
+        lbMakeUpGain[numFilterBands + 1], lbRatio[numFilterBands + 1], lbAttack[numFilterBands + 1],
+        lbRelease[numFilterBands + 1], lbInput, lbOutput;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultiBandCompressorAudioProcessorEditor)
 };
--- source/PluginProcessor.h
+++ source/PluginProcessor.h
@@ -1,36 +1,64 @@
+/*
+ ==============================================================================
+ This file is part of the IEM plug-in suite.
+ Author: Markus Huber
+ Copyright (c) 2017 - Institute of Electronic Music and Acoustics (IEM)
+ https://iem.at
+
+ The IEM plug-in suite is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ The IEM plug-in suite is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this software.  If not, see <https://www.gnu.org/licenses/>.
+ ==============================================================================
+ */
+
 #pragma once
-#include <juce_dsp/juce_dsp.h>
 
-#include "CrossoverProcessor.h"
+#include <juce_audio_processors/juce_audio_processors.h>
 
-class PluginProcessor : public juce::AudioProcessor, public juce::ValueTree::Listener
+#include "juce_dsp/juce_dsp.h"
+#include "AudioProcessorBase.h"
+#include "Compressor.h"
+
+#include "FilterVisualizerHelper.h"
+
+#define ProcessorClass MultiBandCompressorAudioProcessor
+#define numFilterBands 4
+
+using namespace juce::dsp;
+using ParameterLayout = juce::AudioProcessorValueTreeState::ParameterLayout;
+
+class MultiBandCompressorAudioProcessor
+    : public AudioProcessorBase<IOTypes::Ambisonics<>, IOTypes::Ambisonics<>>
 {
 public:
-    PluginProcessor();
+    constexpr static int numberOfInputChannels = 64;
+    constexpr static int numberOfOutputChannels = 64;
+    //==============================================================================
+    MultiBandCompressorAudioProcessor();
+    ~MultiBandCompressorAudioProcessor();
 
-    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
 
-    void processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer&) override;
-
-    void reset() override;
-
-#pragma region JUCE Boilerplate
-    //JUCE
+#ifndef JucePlugin_PreferredChannelConfigurations
     bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+#endif
 
-    using AudioProcessor::processBlock;
+    void processBlock (juce::AudioSampleBuffer&, juce::MidiBuffer&) override;
 
     //==============================================================================
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
-
-    //==============================================================================
-    const juce::String getName() const override;
-
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    bool isMidiEffect() const override;
-    double getTailLengthSeconds() const override;
 
     //==============================================================================
     int getNumPrograms() override;
@@ -38,22 +66,100 @@ public:
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
     void changeProgramName (int index, const juce::String& newName) override;
-    void releaseResources() override;
-    void getStateInformation(juce::MemoryBlock &destData) override;
-    void setStateInformation(const void *data, int sizeInBytes) override;
-    //end JUCE
-#pragma endregion
 
-    juce::AudioProcessorValueTreeState& getAPVTS();
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+
+    //==============================================================================
+    void parameterChanged (const juce::String& parameterID, float newValue) override;
+    void updateBuffers() override;
+
+    //======= Parameters ===========================================================
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> createParameterLayout();
+
+#if JUCE_USE_SIMD
+    using IIRfloat = SIMDRegister<float>;
+    static constexpr int IIRfloat_elements = SIMDRegister<float>::size();
+#else /* !JUCE_USE_SIMD */
+    using IIRfloat = float;
+    static constexpr int IIRfloat_elements = 1;
+#endif
+
+    enum FrequencyBands
+    {
+        Low,
+        MidLow,
+        MidHigh,
+        High
+    };
+
+    // Interface for gui
+    double& getSampleRate() { return lastSampleRate; };
+    IIR::Coefficients<double>::Ptr lowPassLRCoeffs[numFilterBands - 1];
+    IIR::Coefficients<double>::Ptr highPassLRCoeffs[numFilterBands - 1];
+
+    juce::Atomic<bool> repaintFilterVisualization = false;
+    juce::Atomic<float> inputPeak = juce::Decibels::gainToDecibels (-INFINITY),
+                        outputPeak = juce::Decibels::gainToDecibels (-INFINITY);
+    juce::Atomic<float> maxGR[numFilterBands], maxPeak[numFilterBands];
+
+    juce::Atomic<bool> characteristicHasChanged[numFilterBands];
+
+    iem::Compressor* getCompressor (const int i) { return &compressors[i]; };
 
 private:
-    CrossoverProcessor crossover;
+    void calculateCoefficients (const int index);
+    void copyCoeffsToProcessor();
 
-    bool isRestoringState{false};
-    juce::AudioProcessorValueTreeState mAPVTS;
-    juce::AudioProcessorValueTreeState::ParameterLayout createParameters();
-    void valueTreePropertyChanged(juce::ValueTree &treeWhosePropertyHasChanged,
-                                  const juce::Identifier &property) override;
-    std::atomic<bool> mShouldUpdateSpeed{false};
+    inline void clear (juce::dsp::AudioBlock<IIRfloat>& ab);
 
+    double lastSampleRate { 48000 };
+    const int maxNumFilters;
+
+    // list of used audio parameters
+    std::atomic<float>* orderSetting;
+    std::atomic<float>* crossovers[numFilterBands - 1];
+    std::atomic<float>* threshold[numFilterBands];
+    std::atomic<float>* knee[numFilterBands];
+    std::atomic<float>* makeUpGain[numFilterBands];
+    std::atomic<float>* ratio[numFilterBands];
+    std::atomic<float>* attack[numFilterBands];
+    std::atomic<float>* release[numFilterBands];
+    std::atomic<float>* bypass[numFilterBands];
+
+    juce::BigInteger soloArray;
+
+    iem::Compressor compressors[numFilterBands];
+
+    // filter coefficients
+    juce::dsp::IIR::Coefficients<float>::Ptr iirLPCoefficients[numFilterBands - 1],
+        iirHPCoefficients[numFilterBands - 1], iirAPCoefficients[numFilterBands - 1],
+        iirTempLPCoefficients[numFilterBands - 1], iirTempHPCoefficients[numFilterBands - 1],
+        iirTempAPCoefficients[numFilterBands - 1];
+
+    // filters (cascaded butterworth/linkwitz-riley filters + allpass)
+    juce::OwnedArray<IIR::Filter<IIRfloat>> iirLP[numFilterBands - 1], iirHP[numFilterBands - 1],
+        iirLP2[numFilterBands - 1], iirHP2[numFilterBands - 1], iirAP[numFilterBands - 1];
+
+    // data for interleaving audio
+    juce::HeapBlock<char> zeroData;
+    std::vector<juce::HeapBlock<char>> interleavedBlockData;
+    juce::OwnedArray<juce::dsp::AudioBlock<IIRfloat>> interleavedData;
+    juce::dsp::AudioBlock<float> zero;
+    juce::AudioBuffer<float> tempBuffer;
+
+    // filters for processing
+    juce::OwnedArray<juce::dsp::AudioBlock<IIRfloat>> freqBands[numFilterBands];
+    std::vector<juce::HeapBlock<char>> freqBandsBlocks[numFilterBands];
+
+    // Additional compressor parameters
+    float* gainChannelPointer;
+    juce::dsp::AudioBlock<float> gains;
+    juce::HeapBlock<char> gainData;
+
+    juce::Atomic<bool> userChangedFilterSettings = true;
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MultiBandCompressorAudioProcessor)
 };
